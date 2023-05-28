a9892877b525741a884c0a8cb0546aef5db805bb
F53 <fseusb@gmail.com>
2023-05-27 18:33:18 -0600

    Switch to my own WASM xBRZ implementation - fixes dark pixels - improves performance on semitranslucent textures (scaling the alpha channel separately) - better scaling (real xbrz compiled from zenju's source code)

--- process_image.js
+++ process_image.js
@@ -1,6 +1,5 @@
-import {scaleImage} from "https://will-wyx.github.io/xbrz/src/xBRZ.js"
+import {scale} from "https://codef53.github.io/xbrzWA/xbrz.js"
 
-// TODO: Fix Semi-Translucency culling
 /**
  * Asynchronously loads a PNG file, then processes it according to the inputs
  *
@@ -43,17 +42,8 @@ export default async function process_image({ pngFile, scaleFactor, tile, relaye
   canvas = hStack(imgTileEast, canvas, imgTileWest);
   // #endregion
 
-  // #region upscale
-  // split image into alpha and rgb channels
-  let [alphaCanvas, rgbCanvas] = splitRGB_A(canvas);
-
-  // upscale split images
-  alphaCanvas = scaleCanvas(alphaCanvas.getContext('2d'), scaleFactor);
-  rgbCanvas = scaleCanvas(rgbCanvas.getContext('2d'), scaleFactor);
-
-  // merge upscaled images
-  canvas = mergeRGB_A(rgbCanvas, alphaCanvas)
-  // #endregion
+  // upscale
+  canvas = await scale(canvas, scaleFactor);
 
   if (shouldCull) { cullSemiTranslucency(canvas); }
 
@@ -101,47 +91,6 @@ function underlay(canvas, underlayCanvas) {
       }
     }
   }
-}
-
-function mergeRGB_A(rgbCanvas, alphaCanvas) {
-  const ctx = rgbCanvas.getContext('2d');
-  const imageData = ctx.getImageData(0, 0, rgbCanvas.width, rgbCanvas.height);
-  const alphaCtx = alphaCanvas.getContext('2d');
-  const alphaData = alphaCtx.getImageData(0, 0, alphaCanvas.width, alphaCanvas.height);
-
-  for (let i = 0; i < imageData.data.length; i += 4) {
-    const alpha = alphaData.data[i];
-    imageData.data[i + 3] = alpha;
-  }
-
-  ctx.putImageData(imageData, 0, 0);
-
-  return rgbCanvas;
-}
-
-function splitRGB_A(canvas) {
-  const ctx = canvas.getContext('2d');
-  const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
-
-  // Create a grayscale canvas of the alpha channel
-  const alphaCanvas = document.createElement('canvas');
-  alphaCanvas.width = canvas.width;
-  alphaCanvas.height = canvas.height;
-  const alphaCtx = alphaCanvas.getContext('2d');
-  const alphaData = alphaCtx.createImageData(canvas.width, canvas.height);
-
-  for (let i = 0; i < imageData.data.length; i += 4) {
-    const alpha = imageData.data[i + 3];
-    alphaData.data[i] = alpha;
-    alphaData.data[i + 1] = alpha;
-    alphaData.data[i + 2] = alpha;
-    alphaData.data[i + 3] = 255;
-  }
-
-  alphaCtx.putImageData(alphaData, 0, 0);
-  ctx.putImageData(imageData, 0, 0);
-
-  return [alphaCanvas, canvas];
 }
 
 /**
@@ -286,64 +235,4 @@ const tileDict = {
   wrap: (img, direction) => tileWrap(img, direction),
   extend: (img, direction) => tileExtend(img, direction),
   mirror: (img, direction) => tileMirror(img, direction)
-}
-
-/**
- * Scales the provided canvas context using xBRz by the given factor.\
- * Based on https://github.com/will-wyx/xbrz/blob/master/index.html.
- *
- * @param {CanvasRenderingContext2D} ctx - The canvas context to be scaled
- * @param {number} scaleSize - The factor by which to scale the canvas, can be 2, 3, 4, 5, or 6
- * @returns {HTMLCanvasElement} A new canvas element that has been scaled by the given factor
- */
-function scaleCanvas(ctx, scaleSize=4) {
-  // get the canvas image data
-  const { width, height } = ctx.canvas
-  const imageData = ctx.getImageData(0, 0, width, height);
-
-  // convert the image data to a 1D array of pixels
-  const buffer = Array.from(imageData.data);
-  let source = [];
-  for (let i = 0, len = buffer.length; i < len; i += 4) {
-    const r = buffer[i];
-    const g = buffer[i + 1];
-    const b = buffer[i + 2];
-    const a = buffer[i + 3];
-    const pixel = a << 24 | r << 16 | g << 8 | b;
-    source.push(pixel)
-  }
-
-  // scale the image
-  let target = new Array(width * scaleSize * height * scaleSize);
-  target.fill(0);
-  scaleImage(scaleSize, source, target, width, height, 0, height);
-
-  // convert the scaled image back to image data
-  let bufferScale = [];
-  for (let i = 0, len = target.length; i < len; ++i) {
-    const pixel = target[i];
-    const a = (pixel >> 24) & 0xff;
-    const r = (pixel >> 16) & 0xff;
-    const g = (pixel >> 8) & 0xff;
-    const b = (pixel) & 0xff;
-    bufferScale.push(r);
-    bufferScale.push(g);
-    bufferScale.push(b);
-    bufferScale.push(a);
-  }
-
-  const imgScaleBuffer = new Uint8ClampedArray(bufferScale);
-  const widthScale = width * scaleSize;
-  const heightScale = height * scaleSize;
-  const imgScaleData = new ImageData(imgScaleBuffer, widthScale, heightScale);
-
-  // create a new canvas and draw the scaled image to it
-  const canvasScale = document.createElement('canvas');
-  canvasScale.width = widthScale;
-  canvasScale.height = heightScale;
-  const ctxScale = canvasScale.getContext('2d');
-
-  ctxScale.putImageData(imgScaleData, 0, 0)
-
-  return canvasScale
 }
