59e3797ac685913d1dd69446839ad2a07fa57965
F53 <fseusb@gmail.com>
2023-09-21 16:38:42 -0600

    show most recent errors first

--- components/util/Errors.vue
+++ components/util/Errors.vue
@@ -17,25 +17,32 @@ onBeforeUnmount(() => {
 </script>
 
 <template>
-  <div id="errorPanel" class="col">
-    <span v-for="(entry, i) in logs" :key="i">{{ entry }}</span>
+  <div id="errorWrapper">
+    <span id="spacer" />
+    <div id="errorPanel">
+      <span v-for="(entry, i) in logs" :key="i">{{ entry }}</span>
+    </div>
   </div>
 </template>
 
 <style lang="scss">
-#errorPanel {
+#errorWrapper {
+  overflow: auto;
+  display: flex;
+  flex-direction: column-reverse;
+
   position: absolute;
   left: 0; top: 0;
-
   min-width: 20rem;
-  max-width: 100vw;
+  max-width: 60vw;
   max-height: 8rem;
-  overflow-y: scroll;
-
-  > span {
-    font-family: monospace;
-    color: red;
-    background-color: black;
+  > #errorPanel {
+    > span {
+      display: block;
+      font-family: monospace;
+      color: red;
+      background-color: rgba(0, 0, 0, 0.5);
+    }
   }
 }
 </style>
